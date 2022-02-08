0a16dab2fe2e61b18022e877b436bdf580e3747a
Sam Goody <sg@frumtherapist.com>
2022-02-09 01:29:25 +0200

    Handle all variants as described in readme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { default as plugin } from 'tailwindcss/plugin'
 
-const aliases = ['children', 'child'];
+const aliases = {
+    "& > .": ['children', 'child'],
+    "& ."  : ['heir', 'descendant'],
+    "& ~ .": ['sibling'],
+    }
 
-let variants = [
-    ...aliases.map(v => [v, '& > *'])
-    ]
+const variants = Object.entries(aliases).
+    flatMap(([k, v]) => v.map(i => [i, k + i]));
 
 export default plugin(({ addVariant }) =>
     variants.forEach(v => addVariant.apply(null, v)));
